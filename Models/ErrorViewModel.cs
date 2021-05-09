a268af2fc3d2b0f4035f5f8e5bde8dc5e2d89802
gillgillint <82655351+gillgillint@users.noreply.github.com>
2021-05-09 17:45:26 +0700

    test

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace oscar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/resources/js/script.js
@@ -0,0 +1,49 @@
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  $('.js--wp-1').waypoint(function(direction){
+    $('.js--wp-1').addClass('animated fadeIn');
+
+
+  },{
+
+    offset:'50%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction){
+    $('.js--wp-2').addClass('animated fadeInUp');
+
+
+  },{
+
+    offset:'50%'
+  });
+  $('.js--wp-3').waypoint(function(direction){
+    $('.js--wp-3').addClass('animated fadeInUp');
+
+
+  },{
+
+    offset:'50%'
+  });
+
+  $('.js--wp-4').waypoint(function(direction){
+    $('.js--wp-4').addClass('animated fadeInUp');
+
+
+  },{
+
+    offset:'50%'
+  });
